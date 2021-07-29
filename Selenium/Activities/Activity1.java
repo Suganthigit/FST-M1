499c629f7fb60f02e8e7321f0fe8cb03c4db05df
Suganthigit <88105606+Suganthigit@users.noreply.github.com>
2021-07-29 07:00:16 +0530

    Add files via upload
    Selenium Activities

---
+++ Selenium/Activities/Activity1.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+	public class Activity1 {
+	    public static void main(String args[]) throws InterruptedException {
+	        WebDriver driver = new FirefoxDriver();
+	        driver.get("https://www.google.com");
+	        System.out.println(driver.getTitle());
+	       // Thread.sleep(1000);
+	        driver.close();
+	    }
+
+}
